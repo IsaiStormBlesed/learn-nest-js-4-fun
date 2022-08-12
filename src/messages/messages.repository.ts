acc1c3f222cb4d4dbc1bd998814089162f99b8f4
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2022-08-12 19:31:08 +0000

    read & write to file func done

---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,24 @@
+import { writeFile, readFile } from 'fs/promises';
+
+export class MessagesRepository {
+  async findOne(id: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+    return messages[id];
+  }
+
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+    return messages;
+  }
+
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+    const randomID = Math.floor(Math.random() * 999);
+
+    messages[randomID] = { randomID, content };
+    await writeFile('message.json', JSON.stringify(messages));
+  }
+}
